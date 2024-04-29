873b8aee91facc105b903bb8ec4e101aadb01281
imyanyijie <yanyijie1993@gmail.com>
2024-04-29 15:26:35 -0400

    Created functions to create and modify items added form page to create items added page allowing delete items added controllers to add, update, and delete items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,33 +1,158 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
+const Item = require("../models/item");
+const Category = require("../models/category");
+
 exports.item_list = asyncHandler(async (req, res, next) => {
-    res.send("return category list");
+    const items = await Item.find().exec();
+    res.render("item_list",{
+        title: "Item List",
+        items: items,
+    });
 });
 
 exports.item_detail = asyncHandler(async (req, res, next) =>{
-    res.send("specific category"+req.params.id);
+    const item = await Item.findById(req.params.id);
+    res.render("item_detail", {
+        title: "Item detail",
+        item: item,
+    });
 });
 
 exports.item_create_get = asyncHandler(async (req, res, next) =>{
-    res.send("create category get");
+    const categories = await Category.find().exec();
+    for(cat of categories){
+        console.log(cat.name);
+    }
+    console.log(undefined === typeof item);
+    res.render("item_form",{
+        title: "Create Item",
+        categories: categories,
+    });
 });
 
-exports.item_create_post = asyncHandler(async (req, res, next) =>{
+exports.item_create_post =[
+    (req, res, next) => {
+        if (!Array.isArray(req.body.item_category)) {
+          req.body.item_category =
+            typeof req.body.item_category === "undefined" ? [] : [req.body.item_category];
+        }
+        next();
+      },
+    body("item_name", "Item quantity must not be empty")
+        .trim()
+        .isLength({min:1})
+        .escape()
+        .isAlphanumeric()
+        .withMessage("Name should be alphanumeric"),
+    body("item_description")
+        .trim()
+        .isLength({min:3})
+        .escape(),
+    body("item_quantity", "Item quantity must not be empty")
+        .trim()
+        .escape(),
     
-});
+        asyncHandler(async (req, res, next) =>{
+            const error = validationResult(req);
+            const item = new Item ({
+                name: req.body.item_name,
+                description: req.body.item_description, 
+                quantity: req.body.item_quantity, 
+                price: req.body.item_price, 
+                category: req.body.genre,
+            });
+            if(!error.isEmpty()){
+                const categories = await Category.find().exec();
+                res.render("item_form",{
+                    title: "Create Item",
+                    categories: categories,
+                });
+            }
+            else{
+                await item.save();
+                res.redirect(item.url)
+            }
+        })
+    ];
 
 exports.item_update_get = asyncHandler(async (req, res, next) =>{
-    res.send("category update post");
+    const [item,categories] = await Promise.all([
+        Item.findById(req.params.id).exec(),
+        Category.find().exec(),
+    ]);
+    if(item === null){
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+    }
+    res.render("item_form",{
+        title: "Update Item", 
+        item: item, 
+        categories: categories,
+});
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) =>{
+exports.item_update_post = [
+    (req, res, next) => {
+        if (!Array.isArray(req.body.item_category)) {
+          req.body.item_category =
+            typeof req.body.item_category === "undefined" ? [] : [req.body.item_category];
+        }
+        next();
+      },
+    body("item_name", "Item quantity must not be empty")
+        .trim()
+        .isLength({min:1})
+        .escape()
+        .isAlphanumeric()
+        .withMessage("Name should be alphanumeric"),
+    body("item_description")
+        .trim()
+        .isLength({min:3})
+        .escape(),
+    body("item_quantity", "Item quantity must not be empty")
+        .trim()
+        .escape(),
     
-});
+        asyncHandler(async (req, res, next) =>{
+            const error = validationResult(req);
+            const item = new Item ({
+                _id:req.params.id,
+                name: req.body.item_name,
+                description: req.body.item_description, 
+                quantity: req.body.item_quantity, 
+                price: req.body.item_price, 
+                category: req.body.genre,
+            });
+            if(!error.isEmpty()){
+                const categories = await Category.find().exec();
+                res.render("item_form",{
+                    title: "Create Item",
+                    categories: categories,
+                });
+            }
+            else{
+                await Item.findByIdAndUpdate(req.params.id,item);
+                res.redirect(item.url)
+            }
+        })
+    ];
 
 exports.item_delete_get = asyncHandler(async (req, res, next) =>{
-    res.send("category delete post");
+    const item = await Item.findById(req.params.id);
+    if(item === null){
+        console.log("No item found")
+        res.redirect("/item")
+    }
+    res.render("item_delete",{
+        title: "Delete Item",
+        item:item,
+    });
 });
 
 exports.item_delete_post = asyncHandler(async (req, res, next) =>{
+    await Item.findByIdAndDelete(req.body.itemid);
+    res.redirect("/item");
 });
