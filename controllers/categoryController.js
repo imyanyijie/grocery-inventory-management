edea5708f512584103b543af8f657ca55446e008
imyanyijie <yanyijie1993@gmail.com>
2024-04-29 10:56:28 -0400

    Added functions to modify and create Category added page to create and delete categories added functions to create, delete, and update categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,34 +1,135 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 
+const Category = require("../models/category");
+const Item = require("../models/item");
+
 exports.category_list = asyncHandler(async (req, res, next) => {
-    res.send("return category list");
+    const allCategory = await Category.find().exec();
+    res.render("category_list",{
+        title:"Category List",
+        category_list:allCategory,
+    });
 });
 
 exports.category_detail = asyncHandler(async (req, res, next) =>{
-    res.send("specific category"+req.params.id);
+    const [category, items] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({category:req.params.id},"name quantity")
+    ]);
+    console.log("catetory detail")
+    res.render("category_detail",{
+        title: "Category Details",
+        category :category,
+        item_list:items,
+    });
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) =>{
-    res.send("create category get");
+    console.log("create category");
+    res.render("category_form",{
+        title: "Create Category"
+    });
 });
 
-exports.category_create_post = asyncHandler(async (req, res, next) =>{
-    
-});
+exports.category_create_post = [
+    body("category_name")
+        .trim()
+        .isLength({min: 1})
+        .isAlphanumeric()
+        .withMessage("Name should be alphanumeric characters"),
+        asyncHandler(async (req, res, next) =>{
+            const error = validationResult(req);
+            const category = new Category({
+                name:req.body.category_name,
+            });
+            if(!error.isEmpty()){
+                console.log("there is a error save the data");
+                res.render("category_form", {
+                    title: "Create Category", 
+                    category:category,
+                });
+                return;
+            }
+            else{
+                console.log("create post")
+                await category.save();
+                res.redirect(category.url);
+            }
+        })
+    ];
 
 exports.category_update_get = asyncHandler(async (req, res, next) =>{
-    res.send("category update post");
+    const category = await Category.findById(req.params.id).exec();
+    if(category=== null){
+        const err = new Error("Category not found");
+        err.status = 404;
+        return next(err);
+    }
+    res.render("category_form",{title: "Update Category", category: category});
 });
 
-exports.category_update_post = asyncHandler(async (req, res, next) =>{
-    
-});
+exports.category_update_post = [    
+    body("category_name")
+        .trim()
+        .isLength({min: 1})
+        .isAlphanumeric()
+        .withMessage("Name should be alphanumeric characters"),
+    asyncHandler(async (req, res, next) =>{
+        const error = validationResult(req);
+        const category = new Category({
+            name:req.body.category_name,
+            _id:req.params.id,
+        });
+        if(!error.isEmpty()){
+            console.log("there is a error save the data");
+            res.render("category_form", {
+                title: "Update Category", 
+                category:category,
+            });
+            return;
+        }
+        else{
+            console.log("update post")
+            await Category.findByIdAndUpdate(req.params.id, category)
+            res.redirect(category.url);
+        }
+    })
+];
 
 exports.category_delete_get = asyncHandler(async (req, res, next) =>{
-    res.send("category delete post");
+    const[category, categoryItem] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({category: req.params.id},"name quantity")
+    ]);
+    if(category === null){
+        console.log("No category result");
+        res.redirect("/category");
+    }
+    res.render("category_delete",{
+        title: "Delete Category",
+        category: category,
+        category_items: categoryItem
+    });
 });
 
 exports.category_delete_post = asyncHandler(async (req, res, next) =>{
-    
+    console.log("category delete post")
+    const[category, categoryItems] = await Promise.all([
+        Category.findById(req.params.id).exec(),
+        Item.find({category:req.params.id},"name quantity"),
+    ]);
+    if(categoryItems.length>0){
+        res.render("category_delete",{
+            title: "Delete Category",
+            category: category,
+            category_items: categoryItems
+        });
+        return;
+    }
+    else{
+        await Category.findByIdAndDelete(req.body.categoryid);
+        res.redirect("/category");
+    }
+
 });
--- routes/index.js
+++ routes/index.js
@@ -9,39 +9,41 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Store Inventory Manegement System' });
 });
 
-// //category routers//
-// router.get("/category", catagoryController.category_list);
+//category routers//
+router.get("/category/create", catagoryController.category_create_get);
 
-// router.get("/category/:id", catagoryController.category_detail);
+router.post("/category/create", catagoryController.category_create_post);
 
-// router.get("/category/create",catagoryController.category_create_get);
+router.get("/category/:id/update", catagoryController.category_update_get);
 
-// router.post("/category/create", catagoryController.category_create_post);
+router.post("/category/:id/update", catagoryController.category_update_post);
 
-// router.get("/category/:id/update", catagoryController.category_update_get);
+router.get("/category/:id/delete", catagoryController.category_delete_get);
 
-// router.post("/category/:id/update", catagoryController.category_update_post);
+router.post("/category/:id/delete", catagoryController.category_delete_post);
 
-// router.get("/category/:id/delete", catagoryController.category_delete_get);
+router.get("/category/:id", catagoryController.category_detail);
 
-// router.post("/category/:id/delete", catagoryController.category_delete_post);
+router.get("/category", catagoryController.category_list);
 
 
-// //item routers//
-// router.get("/item", itemController.item_list);
 
-// router.get("/item/:id", itemController.item_detail);
 
-// router.get("/item/create",itemController.item_create_get);
+//item routers//
+router.get("/item/create",itemController.item_create_get);
 
-// router.post("/item/create", itemController.item_create_post);
+router.post("/item/create", itemController.item_create_post);
 
-// router.get("/item/:id/update", itemController.item_update_get);
+router.get("/item/:id/update", itemController.item_update_get);
 
-// router.post("/item/:id/update", itemController.item_update_post);
+router.post("/item/:id/update", itemController.item_update_post);
 
-// router.get("/item/:id/delete", itemController.item_delete_get);
+router.get("/item/:id/delete", itemController.item_delete_get);
 
-// router.post("/category/:id/delete", catagoryController.item_delete_post);
+router.post("/item/:id/delete", itemController.item_delete_post);
+
+router.get("/item/:id", itemController.item_detail);
+
+router.get("/item", itemController.item_list);
 
 module.exports = router;
